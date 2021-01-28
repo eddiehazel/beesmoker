f4107ab95df45860c211d381af64e949dfb5b7f1
sig <dan@1up.digital>
2021-01-28 21:02:08 +0000

    bump

--- run-bee.sh
+++ run-bee.sh
@@ -1,8 +1,8 @@
 bee --config /go/src/app/bee-staging.yml start &> /tmp/bee.log &
-# sleep 30
-# ADDRE=$(cat /go/src/app/data/keys/swarm.key | jq .address | tr -d \")
-# echo $ADDRE
-# curl -vv -XPOST https://faucet.ethswarm.org/fund --data token\=$FAUCET_TOKEN\&receiver\=$ADDRE
+sleep 30
+ADDRE=$(cat /go/src/app/data/keys/swarm.key | jq .address | tr -d \")
+echo $ADDRE
+curl -vv -XPOST https://faucet.ethswarm.org/fund --data token\=$FAUCET_TOKEN\&receiver\=$ADDRE
 
 while ! nc -vz localhost 1633
 do
