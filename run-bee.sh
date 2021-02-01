9901eb735c7bc1856e9eabbcd459bf48a4b2745c
sig <dan@1up.digital>
2021-02-01 16:45:02 +0000

    bump

--- run-bee.sh
+++ run-bee.sh
@@ -1,25 +1,23 @@
-curl -vv http://bee-gateway.duckdns.org
+bee --config /go/src/app/bee-staging.yml start &> /tmp/bee.log &
+#sleep 30
+until [ -f /go/src/app/data/keys/swarm.key ]; do echo "wainting for bee"; sleep 3; done
+ADDRE=$(cat /go/src/app/data/keys/swarm.key | jq .address | tr -d \")
+echo $ADDRE
+
 curl -vv -XPOST http://bee-gateway.duckdns.org:9999 --data token\=$FAUCET_TOKEN\&receiver\=$ADDRE
 
-# bee --config /go/src/app/bee-staging.yml start &> /tmp/bee.log &
-# #sleep 30
-# until [ -f /go/src/app/data/keys/swarm.key ]; do echo "wainting for bee"; sleep 3; done
-# ADDRE=$(cat /go/src/app/data/keys/swarm.key | jq .address | tr -d \")
-# echo $ADDRE
-# curl -vv -XPOST https://faucet.ethswarm.org/fund --data token\=$FAUCET_TOKEN\&receiver\=$ADDRE
+curl -vv -XPOST https://faucet.ethswarm.org/fund --data token\=$FAUCET_TOKEN\&receiver\=$ADDRE
 
-# curl -vv -XPOST https://bee-gateway.duckdns.org:9999 --data token\=$FAUCET_TOKEN\&receiver\=$ADDRE
+while ! nc -vz localhost 1633
+do
+  sleep 5
+  tail -n5 /tmp/bee.log
+done
 
-# while ! nc -vz localhost 1633
-# do
-#   sleep 5
-#   tail -n5 /tmp/bee.log
-# done
+go get -d -v ./...
+go install -v ./...
+app
 
-# go get -d -v ./...
-# go install -v ./...
-# app
+echo "------ END OF TEST OUTPUT"
 
-# echo "------ END OF TEST OUTPUT"
-
-# cat /tmp/bee.log
+cat /tmp/bee.log
