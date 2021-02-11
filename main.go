a53ef7aa2de723515e4c53568e73ae905c681796
sig <dan@1up.digital>
2021-02-11 19:07:57 +0000

    update for staging

--- main.go
+++ main.go
@@ -52,9 +52,9 @@ const (
 	tmpFolder = "tmp"
 	getFromTemplate = "https://bee-%d.gateway.staging.ethswarm.org/bytes/%s"
 	maxNode = 19 //presuming they start at 0
-	postSize = 1000
+	postSize = 1000 * 1000 * 0.1
 	maxAttemptsAfterSent = 10	
-	batchSize =  1000
+	batchSize =  100
 	getTestTimoutSecs = 100
 	timeBeforeGetSecs = 60
 	sleepBetweenBatchMs = 300
